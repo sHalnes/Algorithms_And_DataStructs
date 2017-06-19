e8d7125ad91d3f34cc746455f3dc88f5ff711a6b
sHalnes <svetlana.halnes@outlook.com>
2017-06-19 20:09:18 +0200

    linked list

---
+++ linked_list_1.py
@@ -0,0 +1,93 @@
+class Node:
+    def __init__(self, header = None, next = None):
+        self.top = header
+        self.next = next
+
+    def __str__(self):
+        return str(self.top)
+
+    def get_top(self):
+        return self.top
+
+    def get_next(self):
+        return self.next
+
+    def set_next(self, new_link):
+        self.next = new_link
+
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = Node('Sentinel')
+        self.length = 0
+
+    def add_node(self, header):
+        node = Node(header)
+        if self.head.get_next() is None:
+            self.head.set_next(node)
+        else:
+            node.set_next(self.head.get_next())
+            self.head.set_next(node)
+        self.length += 1
+
+    def __str__(self):
+        l = []
+        current_node = self.head.next
+        while current_node is not None:
+            l.append(str(current_node))
+            current_node = current_node.get_next()
+        return('-->'.join(l))
+
+    def insert(self, new_header, add_after_node):
+        current_node = self.head.get_next()
+        new_node = Node(new_header)
+        while current_node.get_next() is not None:
+            current_node = current_node.get_next()
+            if current_node.get_top() == add_after_node:
+                new_node.set_next(current_node.get_next())
+                current_node.set_next(new_node)
+                self.length += 1
+                break
+        if current_node.get_next() is None:
+            print('There is no node {} in the list'.format(add_after_node))
+
+    def has_node(self, target):
+        current_node = self.head.get_next()
+        while current_node.get_next() is not None:
+            if current_node.get_top() == target:
+                return True
+            current_node = current_node.get_next()
+        return False
+
+
+    def get_size(self):
+        return self.length
+
+    def is_empty(self):
+        return self.head.next == None
+
+
+
+ll = LinkedList()
+print('Create linked list')
+ll.add_node(1)
+print('add node 1')
+ll.add_node(2)
+print('add node 2')
+ll.add_node(7)
+print('add node 7')
+ll.add_node(8)
+print('add node 8\n')
+print('\nlist size: ', ll.get_size())
+print(ll)
+print('\ninsert node 5 after node 7')
+ll.insert(5, 7)
+print('list size: ', ll.get_size())
+print(ll)
+print('\ninsert node 5 after node 6')
+ll.insert(5, 6)
+print('\ndo we have node 56?')
+print(ll.has_node(56))
+print('\ndo we have node 7?')
+print(ll.has_node(7))
